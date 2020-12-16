40c7f15931452f6090803b86f74774996a242eb6
Maria Dimansi Chrysandini <chrysan.scorpio@gmail.com>
2020-12-17 06:37:27 +0700

    creating bottom navigation

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -5,8 +5,28 @@ import { HomePage } from './home.page';
 const routes: Routes = [
   {
     path: '',
+    redirectTo: '/home/tabs/map',
+    pathMatch: 'full'
+  },
+  {
+    path: 'tabs',
     component: HomePage,
+    children:[
+      {
+        path: 'friendlist',
+        loadChildren: () => import('./friendlist/friendlist.module').then( m => m.FriendlistPageModule)
+      },
+      {
+        path: 'map',
+        loadChildren: () => import('./map/map.module').then( m => m.MapPageModule)
+      },
+      {
+        path: 'profile',
+        loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+      }
+    ]
   }
+  
 ];
 
 @NgModule({
