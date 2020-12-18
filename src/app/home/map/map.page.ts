48483d3065a6d556ecb8f0af431fa5140bccfa6a
Maria Dimansi Chrysandini <chrysan.scorpio@gmail.com>
2020-12-19 01:58:19 +0700

    check-in toggle and show current location button

--- src/app/home/map/map.page.ts
+++ src/app/home/map/map.page.ts
@@ -1,4 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { ToastController } from '@ionic/angular';
+import { UserService } from 'src/app/services/user.service';
 
 
 declare var google: any;
@@ -10,17 +13,46 @@ declare var google: any;
 })
 export class MapPage implements OnInit {
   map: any;
+  locName: string;
+  isCheckedIn: boolean = false;
+  infoWindow: any = new google.maps.InfoWindow();
   @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
-  umnPos: any = {
-    lat: -6.256081,
-    lng: 106.618755
+  curPos: any = {
+    lat: -6.175392,
+    lng: 106.827153
   };
 
+  constructor(
+    private fireAuth: AngularFireAuth,
+    private userService: UserService,
+    private toastController: ToastController,
+  ) { }
+
+  ngOnInit() {
+  }
+
   ionViewDidEnter(){
-    this.showMap(this.umnPos);
+    this.showMap(this.curPos);
   }
   
+  showCurrentLoc(){
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition((position: Position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        console.log(pos);
+        this.infoWindow.setPosition(pos);
+        this.infoWindow.setContent("Your Current Location");
+        this.infoWindow.open(this.map);
+        this.map.setCenter(pos);
+      });
+    }
+  }
+
   showMap(pos: any){
+    console.log('test Monas', pos);
     const location = new google.maps.LatLng(pos.lat, pos.lng);
     const options = {
       center: location,
@@ -28,17 +60,18 @@ export class MapPage implements OnInit {
       disableDefaultUI: true
     };
     this.map = new google.maps.Map(this.mapRef.nativeElement, options);
-
-    // The marker position
-    const marker = new google.maps.Marker({
-      position: this.umnPos,
-      map: this.map,
-    });
   }
 
-  constructor() { }
-
-  ngOnInit() {
+  addLocation(){
+    var currentTime = new Date().getTime()
+    // this.userService.setUserLocation(this.userUid ,this.userLoc, currentTime);
+    // this.userService.updateCheckinList(this.userUid, this.locName, currentTime, "add");
+    // this.presentToast("Check in added.","success");
+    // this.locName = "";
+    this.toggleLoc();
   }
 
+  toggleLoc(){
+    this.isCheckedIn = !this.isCheckedIn;
+  }
 }
