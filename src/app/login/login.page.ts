99b9fb04825e2bc0271d9147ab71014f60fcca38
Maria Dimansi Chrysandini <chrysan.scorpio@gmail.com>
2020-12-18 11:25:46 +0700

    login layout and its functionality

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { NavController } from '@ionic/angular';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  showPassword = false;
+  passwordToggleIcon = 'eye-off-outline';
+  form: FormGroup;
+  errorMessage = '';
 
-  constructor() { }
+  validationMessages = {
+    email: [
+      { type: 'pattern', message: 'Please input a valid email.' },
+    ],
+    password: [{ type: 'required', message: 'Please input the password.' }],
+  };
+
+  constructor(
+    private navCtrl: NavController,
+    private authSrv: AuthService,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      email: new FormControl(
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'),
+        ])
+      ),
+      password: new FormControl('', Validators.compose([Validators.required])),
+    });
   }
 
+  loginUser() {
+    const value= this.form.value;
+    this.authSrv.loginUser(value).then(
+      (res) => {
+        console.log(res);
+        this.errorMessage = '';
+        this.navCtrl.navigateForward('/home');
+      },
+      (err) => {
+        this.errorMessage = 'Your email or password is wrong.';
+      }
+    );
+  }
+
+  togglePassword(): void {
+    this.showPassword = !this.showPassword;
+    if (this.passwordToggleIcon === 'eye-off-outline') {
+      this.passwordToggleIcon = 'eye-outline';
+    } else {
+      this.passwordToggleIcon = 'eye-off-outline';
+    }
+  }
 }
